6dcc0508d8277098f314a7cde75bb5b6abb4a04e
Tyler Fedrizzi <tyler.fedrizzi@gmail.com>
2020-02-09 19:05:12 -0500

    Update Command class and add research for loading plane module

---
+++ connection.py
@@ -0,0 +1,36 @@
+# Commands for the Plane simulator
+
+import time
+import dronekit
+
+class PlaneCommand:
+    def __init__(self, vehicleConnection):
+        self.vehicle = vehicleConnection
+        self.retry_max = 10
+
+
+    def arm(self):
+        print("Basic pre-arm checks")
+
+        # Don't try to arm until autopilot is ready
+        while not self.vehicle.is_armable:
+            print(" Waiting for connection to initialise...")
+            time.sleep(1)
+
+        print("Arming motors")
+        # Copter should arm in GUIDED mode
+        self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+        self.vehicle.armed = True
+        
+        try_number = 0
+        # Confirm vehicle armed before attempting to take off
+        while not (self.vehicle.armed or try_number == self.retry_max):
+            print(" Waiting for arming...")
+            try_number += 1
+            time.sleep(1)
+        
+        return True
+        
+    def takeoff(self, targetAltitude):
+        print("Taking off!")
+        self.vehicle.simple_takeoff(targetAltitude) # Take off to target altitude
--- main.py
+++ main.py
@@ -1,42 +1,22 @@
 import dronekit_sitl, dronekit, time
 from route import Route
-from connection import connection
+from connection import PlaneCommand
 
 WAPOINT_FILE_PATH = 'waypoints.json'
+TARGET_ALTITUDE = 20
 
-def arm_and_takeoff(aTargetAltitude):
-    print("Basic pre-arm checks")
-
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
-
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = dronekit.VehicleMode("GUIDED")
-    vehicle.armed = True
-
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
-
-    print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
-
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
-    while True:
-        print " Altitude: ", vehicle.location.global_relative_frame.alt
-        #Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
-            print "Reached target altitude"
-            break
-        time.sleep(1)
+def printStateData(vehicle):
+    print("Get some vehicle attribute values:")
+    print(" GPS: %s" % vehicle.gps_0)
+    print(" Battery: %s" % vehicle.battery)
+    print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
+    print(" Is Armable?: %s" % vehicle.is_armable)
+    print(" System status: %s" % vehicle.system_status.state)
+    print(" Mode: %s" % vehicle.mode.name)
         
 # Starting the flight simulator
 print("Start simulator (SITL)")
+# sitl.launch([])
 sitl = dronekit_sitl.start_default()
 connection_string = sitl.connection_string()
 
@@ -44,22 +24,33 @@ route = Route(WAPOINT_FILE_PATH)
 
 # Connect to the Vehicle.
 print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = dronekit.connect(connection_string, wait_ready=True)
+vehicleConnection = dronekit.connect(connection_string, wait_ready=False)
 
-# Get some vehicle attributes (state)
-print("Get some vehicle attribute values:")
-print(" GPS: %s" % vehicle.gps_0)
-print(" Battery: %s" % vehicle.battery)
-print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
-print(" Is Armable?: %s" % vehicle.is_armable)
-print(" System status: %s" % vehicle.system_status.state)
-print(" Mode: %s" % vehicle.mode.name)
+plane = PlaneCommand(vehicleConnection)
 
-arm_and_takeoff(20)
+printStateData(plane.vehicle)
+
+# Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+        #  after Vehicle.simple_takeoff will execute immediately).
+
+printStateData(plane.vehicle)
+
+plane.arm()
+plane.takeoff(TARGET_ALTITUDE)
+
+while True:
+    print(" Altitude: %s" % plane.vehicle.location.global_relative_frame.alt)
+    #Break and return from function just below target altitude.
+    if plane.vehicle.location.global_relative_frame.alt>=TARGET_ALTITUDE*0.95:
+        print("Reached target altitude")
+        break
+    time.sleep(1)
+
+printStateData(plane.vehicle)
 
 # Close vehicle object before exiting script
-vehicle.close()
+plane.vehicle.close()
 
 # Shut down simulator
-sitl.stop()
+# sitl.stop()
 print("Completed")
