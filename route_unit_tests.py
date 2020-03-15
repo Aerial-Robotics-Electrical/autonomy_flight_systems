0d77f1e0cfb08e7c430b0f7c18c89edb4417efac
Cole Kniffen <ckniffen@purdue.edu>
2020-03-15 00:40:21 -0400

    Removing waypoints.json due to more comprehensive mission example, simplifying unit test for route

--- route_unit_tests.py
+++ route_unit_tests.py
@@ -8,7 +8,6 @@ class TestRouteMethods(unittest.TestCase):
 
     def test_change_routes(self):
         newRoute = self.route.changeRoute(self.route.waypoints, [38.1417722222222, -76.4251083333333, 400.0], [[0,0,0], [0,0,0]])
-        self.assertEqual(5, len(newRoute))
         self.assertEqual([[38.1446916666667, -76.4279944444445, 200.0], 
                         [38.1461944444444, -76.4237138888889, 300.0], 
                         [38.1438972222222, -76.42255, 400.0], 
