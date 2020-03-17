d579c8c5b97162c2f7077d9f774d75ce59d36285
Cole Kniffen <ckniffen@purdue.edu>
2020-03-16 20:22:30 -0400

    unit test fix

--- route_unit_tests.py
+++ route_unit_tests.py
@@ -7,7 +7,7 @@ class TestRouteMethods(unittest.TestCase):
         self.route = Route("interop_example.json") #data for unit test in this file
 
     def test_change_routes(self):
-        newRoute = self.route.changeRoute(self.route.waypoints, [38.1417722222222, -76.4251083333333, 400.0], [[0,0,0], [0,0,0]])
+        newRoute = self.route.changeRoute(self.route.data["waypoints"], [38.1417722222222, -76.4251083333333, 400.0], [[0,0,0], [0,0,0]])
         self.assertEqual([[38.1446916666667, -76.4279944444445, 200.0], 
                         [38.1461944444444, -76.4237138888889, 300.0], 
                         [38.1438972222222, -76.42255, 400.0], 
