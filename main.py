1f0d038a8c0b96ebda9f92dc45d9d548f893934b
Tyler Fedrizzi <tyler.fedrizzi@gmail.com>
2020-03-17 07:42:19 -0400

    Add changes for recording flight data

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from plane_commands import PlaneCommand
 from map_route import MapRoute
 from mission import Mission
 import json
+import csv
 
+# Global variables
 WAPOINT_FILE_PATH = 'waypoints.json'
 MAP_PATH = 'resources/lafayette_map_2.png'
 TARGET_ALTITUDE = 60
 TARGET_LATITUDE = 40.373434
 TARGET_LONGITUDE = -86.866277
 CONNECTION_STRING = 'tcp:127.0.0.1:5760'
+FLIGHT_DATA_FILE_NAME = 'test_flight_03_12_2020'
 
 def printStateData(vehicle):
     print("Get some vehicle attribute values:")
@@ -23,10 +26,19 @@ def printStateData(vehicle):
     print(" Mode: %s" % vehicle.mode.name)
     print(" Ground Speed: %s" % vehicle.groundspeed)
     print(" Heading: %s" % vehicle.heading)
+
+def create_flight_data_log(flight_data, file_name):
+    with open(file_name + '.csv', 'w') as f:
+        for key in flight_data.keys():
+            f.write("%s\t"%(key))
+        f.write("\n")
+        for lat, lon, speed in zip(flight_data['latitude'], flight_data['longitude'], flight_data['ground_speed']):
+            f.write("%s\t%s\t%s"%(lat, lon, speed))
+            f.write("\n")
         
 # Start the dronekit-sitl plane simulator utilizing the following command:
 # dronekit-sitl ./../ardupilot/build/sitl/bin/arduplane --home=lat,lon,altitude,heading(yaw) --model=plane
-# --defaults PATH_TO_DIRECTORY/plane.param
+# --defaults PATH_TO_DIRECTORY/plane.parm
 
 # Connect to the Vehicle.
 print("Connecting to vehicle on: %s" % (CONNECTION_STRING,))
@@ -41,6 +53,7 @@ printStateData(plane.vehicle)
 file = open(WAPOINT_FILE_PATH, 'rb')
 waypoint_list = json.loads(file.read())
 
+# Generate the take-off mission
 mission_1 = Mission(waypoint_list["waypoints"], 'infil', takeoff_required=True)
 mission_1.generate_intermediate_waypoints()
 mission_1.add_take_off_command()
@@ -77,6 +90,7 @@ minutes = total_time / 60
 seconds = total_time - (minutes * 60)
 print("Total Route Time: {mins} mins {secs} secs".format(mins=round(minutes, 4), secs=round(seconds, 4)))
 
+create_flight_data_log(flight_data, FLIGHT_DATA_FILE_NAME)
 printStateData(plane.vehicle)
 
 # Close vehicle object before exiting script
